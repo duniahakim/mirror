286594db490bcf04803fed065016eab8a9bc5863
Michelle <vinocour@stanford.edu>
2021-05-15 20:28:55 -0700

    edit profile

--- js/profileinfo.js
+++ js/profileinfo.js
@@ -2,11 +2,34 @@
 
 var db = firebase.firestore();
 
-const add_item_button = document.querySelector("#add_item_button");
-const item_title = document.querySelector("#new_item_title_textbox");
-const item_price = document.querySelector("#new_item_price_textbox");
-const item_size = document.querySelector("#new_item_size_textbox");
-const item_condition = document.querySelector("#new_item_condition_textbox");
-const item_brand = document.querySelector("#new_item_brand_textbox");
-const item_color = document.querySelector("#new_item_color_textbox");
+const first_name_textbox = document.querySelector("#first_name_textbox");
+const last_name_textbox = document.querySelector("#last_name_textbox");
+const email_textbox = document.querySelector("#email_textbox");
+const venmo_username = document.querySelector("#venmo_username");
+const about = document.querySelector("#about");
+
+var users_collection_ = db.collection("users");
+var user_info = db.collection("users").doc("profileinfo");
+
+save_changes.addEventListener("click", function() {};
+  const first_value = first_name_textbox.value;
+  const last_value = last_name_textbox.value;
+  const email_value = email_textbox.value;
+  const venmo_value = venmo_username.value;
+  const about_value = about.value;
+
+   user_info.update({
+    name: first_value,
+    last name: last_value,
+    email: email_value,
+    venmo: venmo_value,
+    about: about_value,
+
+  }).then((docRef) => { //this adds the item to the user's closet
+    console.log("Item written with ID: ", docRef.id);
+  }).catch((error) => {
+    console.error("Error adding value: ", error);
+  });
+});
+
 
