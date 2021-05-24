6d8c002be44e81345cb3daaf522bf8b18e191ffe
duniahakim <doniahakim@gmail.com>
2021-05-23 23:38:03 -0700

    test

--- js/add_new_item.js
+++ js/add_new_item.js
@@ -89,7 +89,7 @@ firebase.auth().onAuthStateChanged(function(user) {
                 sold: false
               }).then((docRef) => { //this adds the item to the user's closet
                 db.collection("data").doc("added_items").update({
-                  number: db.FieldValue.increment(1)
+                  number: 1
                 }).then((docRef)=> {
                   console.log("items added incremented!");
                 }).catch((error) => {
